fb5c4d7e653e8ee3b3dced2dc181526672d7e381
Yavuzhan Albayrak <yavuzalbayrak55@gmail.com>
2025-07-02 11:50:15 +0300

    fix: claims uncommented in Authenticated user

--- src/DailyDN.Application/Services/IAuthenticatedUser.cs
+++ src/DailyDN.Application/Services/IAuthenticatedUser.cs
@@ -1,9 +1,11 @@
+using DailyDN.Domain.Entities;
+
 namespace DailyDN.Application.Services
 {
     public interface IAuthenticatedUser
     {
         public int UserId { get; set; }
         public bool IsAuthenticated { get; set; }
-        // public List<Claim> Claims { get; set; }
+        public List<Claim> Claims { get; set; }
     }
 }
--- src/DailyDN.Application/Services/Impl/AuthenticatedUser.cs
+++ src/DailyDN.Application/Services/Impl/AuthenticatedUser.cs
@@ -1,9 +1,11 @@
+using DailyDN.Domain.Entities;
+
 namespace DailyDN.Application.Services.Impl
 {
     public class AuthenticatedUser : IAuthenticatedUser
     {
         public int UserId { get; set; }
         public bool IsAuthenticated { get; set; }
-        // public List<Claim> Claims { get; set; }
+        public List<Claim> Claims { get; set; } = [];
     }
 }
